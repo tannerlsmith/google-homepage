ad34e71d024e6f38aa51d74e29f4daf2ae941de3
tannerlsmith <tanner.smith@uvm.edu>
2021-08-29 10:11:20 -0400

    added uvi

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 // vars where I store things
 var apiKey = '6a370bbd49c72304c89b6cb94973905c';
-
-
-// click the select city function
 var city = "";
+
 $('.enter-button').on('click', function (event) {
     event.preventDefault();
-    // gets value of the city input to correctly make the call.
     var city = $('#city').val();
 
-    https://openweathermap.org/api/one-call-api
-    // calls the json 
     $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial", function(data) {
         console.log(data)
         let currentIconEl = $('.current-icon') 
@@ -29,41 +24,14 @@ $('.enter-button').on('click', function (event) {
         currentIconEl.html(data.name + ' ' + date + ' <img src='+ currentIconLink+'> ')
         currentWindEl.html('Wind speed: ' + data.wind.speed)
 
-        
-            
-        // links to the icon emoji when we call the icon, + whatever code the weather is [0] is an array with icons.
-        // this will link the temperature site, & this gives us the temp number.
-        // var temp = Math.floor(data.main.temp);
-        // This adds the weather description.
-        // var weather = data.weather[0].main;
-            
-        // puts the icon inside the icon element.
-        // $('.icon').attr('src', icon);
-        // puts weather description inside weather element.
-        // $('.weather').append(weather);
-        // puts the temp inside the temp element.
-        // $('.temp').append(temp);
-            
-            
-        // trying to get it to display
-        // var displayContainerEl = document.querySelector('#data-container');
-        // displayContainerEl.innerHTML = '';
-        // displayContainerEl.appendChild(data);
-        
-
-        // converts the temperature
-        // when i figure it out...        
         getUVIndex(lat, lon)
-        
     })
-    
-    
-    
 })
 
 function getUVIndex(lat, lon) {
     $.getJSON('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + apiKey, function(data) {
         console.log('UV INDEX one call API', data)
-    }
-    )
+        let getUVI = $('current-UV')
+        getUVI.html('UVI: ' + data.current.uvi)
+    })
 }
