5dbb51f90584fc7fdc8bca5ff27ff36d0467ab6c
tannerlsmith <tanner.smith@uvm.edu>
2021-08-31 15:01:54 -0400

    added more functions w/ 5 day forecast

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 // vars where I store things
 var apiKey = '6a370bbd49c72304c89b6cb94973905c';
 var city = "";
+var searchesEl = $('.searches');
 
 $('.enter-button').on('click', function (event) {
     event.preventDefault();
     var city = $('#city').val();
+    $(this).closest('form').find('input[type=text], textarea').val('')
 
     $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial", function(data) {
-        console.log(data)
         let currentIconEl = $('.current-icon') 
-        let currentWeatherEl = $('.current-weather')
         let currentTempEl = $('.current-temp') 
         let currentHumidityEl = $('.current-humidity')
-        let currentUvEl = $('.current-UV')
         let date = moment.unix(data.dt).format('MM/DD/YYYY')
         var currentIconLink = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
         let currentWindEl = $(".current-wind")
@@ -26,12 +25,50 @@ $('.enter-button').on('click', function (event) {
 
         getUVIndex(lat, lon)
     })
+
+    // call search function
+    searchSection(city)
 })
 
 function getUVIndex(lat, lon) {
-    $.getJSON('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + apiKey, function(data) {
-        console.log('UV INDEX one call API', data)
-        let getUVI = $('current-UV')
-        getUVI.html('UVI: ' + data.current.uvi)
+    $.getJSON('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly,alerts&appid=' + apiKey, function(data) {
+        let getUVIEl = $('.current-UV')
+        getUVIEl.html('UVI: ' + data.current.uvi)
+
+        fiveDayForecast(data.daily)
     })
 }
+
+function fiveDayForecast(dailyForecast) {
+    console.log('This is a daily forecast', dailyForecast)
+
+    for (i = 1; i < 6; i++) {
+        let fiveDayEl = $('#day-' + i)
+
+        // clears cities
+        fiveDayEl.html('')
+
+        let dateEl = $('<p> </p>').html(moment.unix(dailyForecast[i].dt).format('MM/DD/YYYY'))
+        
+        var iconLinkEl = "https://openweathermap.org/img/w/" + dailyForecast[i].weather[0].icon + ".png"
+        let iconEl = $('<img class="jquery-img" src='+ iconLinkEl + '>')
+        
+        let tempEl = $('<p> </p>').html("Temperature: " + dailyForecast[i].temp.day)
+
+        let windEl = $('<p> </p>').html('Wind Speed: ' + dailyForecast[i].wind_speed)
+
+        let humidityEl = $('<p> </p>').html('Humidity: ' + dailyForecast[i].humidity + ' %')
+        
+        fiveDayEl.append(dateEl, iconEl, tempEl, windEl, humidityEl)
+    }
+}
+
+function searchSection(city) {
+    var liEl = $('<li> </li>').html(city)
+    searchesEl.append(liEl)
+}
+
+
+
+// Date, Icon, Temp, Wind, Humidity
+// weather.[0].icon
